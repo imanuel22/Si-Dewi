52887aafc3e2a0b72837d357495a42b4452bfa74
imanuel22 <imanuelrenra220@gmail.com>
2024-07-25 21:02:14 +0800

    fitur kontol

--- resources/views/guest/layouts/navbar.blade.php
+++ resources/views/guest/layouts/navbar.blade.php
@@ -65,7 +65,7 @@
                         </div>
                         <input type="text" id="search-navbar" name="search"
                             class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search...">
+                            placeholder="Search..." value="{{ request()->get('search') }}">
                     </div>
                     <button data-collapse-toggle="navbar-search" type="button"
                         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
