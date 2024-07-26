67e460ba6f1fd8328cf1e7c910771be1b336f1d7
imanuel22 <imanuelrenra220@gmail.com>
2024-07-26 12:17:47 +0800

    Update recent_post.blade.php

--- resources/views/guest/artikel/recent_post.blade.php
+++ resources/views/guest/artikel/recent_post.blade.php
@@ -3,7 +3,8 @@
         <div class="grid grid-cols-1 gap-4 col-span-2 row-start-2 mx-auto w-full mb-10">
             @foreach ($berita as $row)
                 <a href="/artikel/{{ $row['id'] }}">
-                    <div class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div
+                        class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <article class="">
                             <div class="border-black">
                                 <div class="">
@@ -11,11 +12,12 @@
                                         src="{{ env('APP_API_URL') }}/resource/berita/{{ $row['gambar'] }}"
                                         alt="{{ $row['judul'] }}">
                                 </div>
+
                                 <div class="headlightfont-normal text-gray-700 dark:text-gray-400 break-words p-5 pt-7">
                                     <h5 class="pt-blog-title font-bold"><a
                                             href="/berita/{{ $row['slug'] }}">{{ $row['judul'] }}</a>
                                     </h5>
-                                    <p>{{Str::limit($row['isi_berita'], 500) }}</p>
+                                    <p>{{ Str::limit($row['isi_berita'], 500) }}</p>
                                 </div>
                             </div>
                         </article>
