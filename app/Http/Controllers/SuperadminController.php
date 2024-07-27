834e7e14ee407597b1ca61eabb4e1580fedb59eb
imanuel22 <imanuelrenra220@gmail.com>
2024-07-28 01:23:09 +0800

    a

--- app/Http/Controllers/SuperadminController.php
+++ app/Http/Controllers/SuperadminController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class SuperadminController extends Controller
 {
     public function index(){
-        
+        $desa = Http::withToken(request()->session()->get('accessToken'))->get(env('APP_API_URL').'/desawisata')->collect();
+        $desaPerKabupaten = $desa->groupBy('kabupaten')->map->count();
+
+        $data = [
+            'desa'=>$desa,
+            'desa_count'=>$desa->count(),
+            'desa_per_kabupaten'=>$desaPerKabupaten,
+        ];
+        return view('superadmin.dashboard',$data);
     }
 }
--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -1,6 +1,19 @@
 @extends('superadmin.layouts.main')
 
 @section('main')
-<h1>Halaman dashboard</h1>
-
+    <h1>Halaman dashboard</h1>
+    <div class="">
+        <p>count desa: {{ $desa_count }}</p>
+    </div>
+    <div class="">
+        <p>count desa di Badung: {{ $desa_per_kabupaten['Badung'] }}</p>
+        <p>count desa di Bangli: {{ $desa_per_kabupaten['Bangli'] }}</p>
+        <p>count desa di Jembrana: {{ $desa_per_kabupaten['Jembrana'] }}</p>
+        <p>count desa di Klungkung: {{ $desa_per_kabupaten['Klungkung'] }}</p>
+        <p>count desa di Karangasem: {{ $desa_per_kabupaten['Karangasem'] }}</p>
+        <p>count desa di Gianyar: {{ $desa_per_kabupaten['Gianyar'] }}</p>
+        <p>count desa di Tabanan: {{ $desa_per_kabupaten['Tabanan'] }}</p>
+        <p>count desa di Denpasar: {{ $desa_per_kabupaten['Denpasar'] }}</p>
+        <p>count desa di Buleleng: {{ $desa_per_kabupaten['Buleleng'] }}</p>
+    </div>
 @endsection
