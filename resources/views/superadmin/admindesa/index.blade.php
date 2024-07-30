25959f4a5d7a23dc82cae6932a7233e8ff84e771
Juventuss <134345238+MartabakEnak@users.noreply.github.com>
2024-07-30 15:45:28 +0800

    Uopdate Admin
    Uopdate Admin

--- resources/views/superadmin/admindesa/index.blade.php
+++ resources/views/superadmin/admindesa/index.blade.php
@@ -1,8 +1,10 @@
 @php
-     use Carbon\Carbon;
+    use Carbon\Carbon;
 @endphp
 @extends('superadmin.layouts.main')
+<style>
 
+</style>
 @section('main')
     @if (session()->has('message'))
         <div id="toast-success"
@@ -18,7 +20,7 @@
                 <span class="sr-only">Check icon</span>
             </div>
             <div class="ms-3 text-sm font-normal">{{ session('message') }}</div>
-            <button type="button"
+            <button type="button" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
                 class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
                 data-dismiss-target="#toast-success" aria-label="Close">
                 <span class="sr-only">Close</span>
@@ -32,51 +34,106 @@
     @endif
     {{-- @dd($admindesa) --}}
 
-
+    {{-- Form --}}
+    <div tabindex="-1" aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        id="authentication-modal">
+        <div class="relative w-full max-w-md max-h-full zoom-in">
+            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
+                        Tambah Petugas Desa
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-toggle="authentication-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <form class="p-4 md:p-5" action="/superadmin/admindesa" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-5">
+                        <label for="id_akun" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
+                            Akun
+                            Petugas
+                        </label>
+                        <select id="id_akun" name="id_akun"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option hidden>Piih Akun</option>
+                            @foreach ($akun as $row)
+                                <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-5">
+                        <label for="id_desawisata"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
+                            Desa Wisata</label>
+                        <select id="id_desawisata" name="id_desawisata"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option hidden>Pilih Desa</option>
+                            @foreach ($desawisata as $row)
+                                <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button type="submit"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <button type="reset"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reset</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- Table --}}
     <div class="mx-4">
         <h2 class="mb-3  font-bold tracking-tight text-gray-500 ">Petugas / Table </h2>
         <div class="flex justify-between">
             <h1 class=" text-3xl font-semibold tracking-tight text-gray-900 dark:text-white"> Petugas Desa Wisata</h1>
-            <a href="/superadmin/admindesa/create"
+            <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal"
                 class="text-white bg-primary-200 hover:bg-primary-100 focus:ring-4 focus:ring-teal-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-200 darl:hover:bg-primary-100  dark:focus:ring-teal-200 focus:outline-none ">Tambah
-                +</a>
+                +</button>
         </div>
-        <div class="relative overflow-x-auto mt-5 bg-white border-gray-200 rounded-lg shadow p-3">
-            <table id="myTable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs  text-gray-700 uppercase  dark:bg-gray-700 dark:text-gray-400">
-                    <tr class="text-center">
-                        <th scope="col" class="px-6 py-3  border w-12">
+        <div class="relative p-3 mt-5 overflow-x-auto bg-white border-gray-200 rounded-lg shadow">
+            <table id="myTable" class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400 table-fixed">
+                <thead class="text-xs text-gray-700 uppercase dark:bg-gray-700 dark:text-gray-400">
+                    <tr class="">
+                        <th scope="col" class="w-12 px-2">
                             No
                         </th>
-                        <th scope="col" class="border px-6 py-3  w-44 text-center">
+                        <th scope="col" class="w-1/5">
                             Desa Wisata
                         </th>
-                        <th scope="col" class=" border px-6 py-3 text-center">
+                        <th scope="col" class=" w-1/5  ">
                             Akun
                         </th>
-                        <th scope="col" class="border px-6 py-3 text-center">
+                        <th scope="col" class=" w-1/5 ">
                             createdAt
                         </th>
-                        <th scope="col" class="border px-6 py-3 text-center">
+                        <th scope="col" class="  w-1/5">
                             updatedAt
                         </th>
-                        <th scope="col" class="border px-6 py-3 text-center">
+                        <th scope="col" class="w-1/5 ">
                             Action
                         </th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($admindesa as $row)
-                        <tr
-                            class="text-center bg-white dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                            <th scope="row"
-                                class="border px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <tr class=" bg-white dark:bg-gray-800  hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="  font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ $loop->iteration }}
                             </th>
-                            <td class="border px-6 py-4">
+                            <td class=" ">
                                 {{ $row['desa']['nama'] }}
                             </td>
-                            <td class="border px-6 py-4">
+                            <td class=" ">
                                 {{ $row['akun']['nama'] }}
                             </td>
                             @php
@@ -85,42 +142,40 @@
                                 $formattedCreatedAt = $created->format('d F Y, H:i');
                                 $formattedUpdatedAt = $updated->format('d F Y, H:i');
                             @endphp
-                            <td class="border px-6 py-4">
+                            <td class=" ">
                                 {{ $formattedCreatedAt }}
                             </td>
-                            <td class="border px-6 py-4">
+                            <td class=" ">
                                 {{ $formattedUpdatedAt }}
                             </td>
-                            <td class="border px-6 py-4 ">
-                                <div class="flex justify-center">
-                                    {{-- <a href="/superadmin/admindesa/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">S</a> --}}
-                                    <a href="/superadmin/admindesa/{{ $row['id'] }}/edit"
-                                        class=" focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><svg
+                            <td class="py-4 ">
+                                <div class="flex justify-center items-center">
+                                {{-- <a href="/superadmin/desa/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">S</a> --}}
+                                <a href="/superadmin/admindesa/{{ $row['id'] }}/edit"
+                                    class=" focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><svg
+                                        class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z"
+                                            clip-rule="evenodd" />
+                                        <path fill-rule="evenodd"
+                                            d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z"
+                                            clip-rule="evenodd" />
+                                    </svg></a>
+                                <form action="/superadmin/admindesa/{{ $row['id'] }}" method="post"
+                                    onsubmit="return confirm('yakin')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"><svg
                                             class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                             width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                             <path fill-rule="evenodd"
-                                                d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z"
+                                                d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
                                                 clip-rule="evenodd" />
-                                            <path fill-rule="evenodd"
-                                                d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z"
-                                                clip-rule="evenodd" />
-                                        </svg></a>
-                                    <form action="/superadmin/admindesa/{{ $row['id'] }}" method="post"
-                                        class="delete-form">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button"
-                                            class= "  focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 delete-button">
-                                            <svg class="w-6 h-6 text-white" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                fill="currentColor" viewBox="0 0 24 24">
-                                                <path fill-rule="evenodd"
-                                                    d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
-                                    </form>
-                                </div>
+                                        </svg></button>
+                                </form>
+                            </div>
                             </td>
                         </tr>
                     @endforeach
