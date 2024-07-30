cac660537f00900acde62c2b25bbd22a8e8fda57
imanuel22 <imanuelrenra220@gmail.com>
2024-07-30 15:57:54 +0800

    perbaiki login

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
@@ -17,6 +18,11 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
 
+        $admindesa = Http::withToken($request->session()->get('accessToken'))->get(env('APP_API_URL').'/admindesa/akun/'.$request->session()->get('id'))->collect();
+
+        Session::put([
+            'id_desa'=>$admindesa[0]['id_desawisata'],
+        ]);
         if(!$request->session()->get('accessToken')){
             return redirect('/login');
         }
