5fba76aa92b4da3c945e6e3bb011c0ab65dcb6a6
Juventuss <134345238+MartabakEnak@users.noreply.github.com>
2024-08-01 02:07:48 +0800

    a

--- resources/views/Admin/fasilitas/show.blade.php
+++ resources/views/Admin/fasilitas/show.blade.php
@@ -90,9 +90,9 @@
                                                 clip-rule="evenodd" />
                                         </svg></button>
                                     {{-- <a href="/superadmin/desa/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">S</a> --}}
-                                    <form action="/admin/fasilitas/{{ $row['id'] }}" method="post">
-                                        @method('DELETE')
+                                    <form action="/admin/fasilitas/{{ $row['id'] }}" method="post" class="delete-form">
                                         @csrf
+                                        @method('DELETE')
                                         <input type="hidden" name="id_destinasiwisata" value="{{$row['id_destinasiwisata']}}" >
                                         <button type="submit"
                                             class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 delete-button">
