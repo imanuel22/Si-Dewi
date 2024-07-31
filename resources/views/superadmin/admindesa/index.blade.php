6ea18008c2631059eeaf500346db006713dc9809
Juventuss <134345238+MartabakEnak@users.noreply.github.com>
2024-07-31 23:39:19 +0800

    edit

--- resources/views/superadmin/admindesa/index.blade.php
+++ resources/views/superadmin/admindesa/index.blade.php
@@ -92,63 +92,7 @@
         </div>
     </div>
 
-    {{-- Edit --}}
-    <div tabindex="-1" aria-hidden="true"
-        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-        id="edit-modal">
-        <div class="relative w-full max-w-md max-h-full zoom-in">
-            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-700">
-                <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
-                        Tambah Petugas Desa
-                    </h3>
-                    <button type="button"
-                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-toggle="edit-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg> 
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <form class="p-4 md:p-5" action="/superadmin/admindesa" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="mb-5">
-                        <label for="id_akun" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
-                            Akun
-                            Petugas
-                        </label>
-                        <select id="id_akun" name="id_akun"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option hidden>Piih Akun</option>
-                            @foreach ($akun as $row)
-                                <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="mb-5">
-                        <label for="id_desawisata"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
-                            Desa Wisata</label>
-                        <select id="id_desawisata" name="id_desawisata"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option hidden>Pilih Desa</option>
-                            @foreach ($desawisata as $row)
-                                <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <button type="submit"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                    <button type="reset"
-                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reset</button>
-                </form>
-            </div>
-        </div>
-    </div>
+
     {{-- Table --}}
     <div class="mx-4">
         <h2 class="mb-3  font-bold tracking-tight text-gray-500 ">Petugas / Table </h2>
@@ -210,7 +154,7 @@
                             <td class="py-4 ">
                                 <div class="flex justify-center items-center">
                                     {{-- <a href="/superadmin/desa/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">S</a> --}}
-                                    <button data-modal-target="edit-modal" data-modal-toggle="edit-modal"
+                                    <button data-modal-target="edit-modal{{$row['id']}}" data-modal-toggle="edit-modal{{$row['id']}}"
                                         class=" focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><svg
                                             class="w-6 h-6 text-white" aria-hidden="true"
                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24"
@@ -239,6 +183,66 @@
                                 </div>
                             </td>
                         </tr>
+
+                        {{-- Edit --}}
+                        <div tabindex="-1" aria-hidden="true"
+                            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                            id="edit-modal{{$row['id']}}">
+                            <div class="relative w-full max-w-md max-h-full zoom-in">
+                                <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Modal header -->
+                                    <div
+                                        class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-2xl font-semibold text-gray-900 dark:text-white">
+                                            {{$row['id']}}
+                                        </h3>
+                                        <button type="button"
+                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                            data-modal-toggle="edit-modal{{$row['id']}}">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <form class="p-4 md:p-5" action="/superadmin/admindesa" method="POST"
+                                        enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="mb-5">
+                                            <label for="id_akun"
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> {{ $row['desa']['nama'] }}
+                                            </label>
+                                            <select id="id_akun" name="id_akun"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                <option hidden>Piih Akun</option>
+                                                @foreach ($akun as $row)
+                                                    <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="mb-5">
+                                            <label for="id_desawisata"
+                                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih
+                                                Desa Wisata</label>
+                                            <select id="id_desawisata" name="id_desawisata"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                <option hidden>Pilih Desa</option>
+                                                @foreach ($desawisata as $row)
+                                                    <option value="{{ $row['id'] }}">{{ $row['nama'] }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <button type="submit"
+                                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                                        <button type="reset"
+                                            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reset</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </tbody>
             </table>
