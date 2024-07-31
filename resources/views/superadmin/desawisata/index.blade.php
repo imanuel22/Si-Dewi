7446489ae4ec95f62be9a024056fb4b9c7e31263
Juventuss <134345238+MartabakEnak@users.noreply.github.com>
2024-08-01 01:03:23 +0800

    front end

--- resources/views/superadmin/desawisata/index.blade.php
+++ resources/views/superadmin/desawisata/index.blade.php
@@ -125,7 +125,7 @@
                             <td class="px-6  py-4 ">
                                 <div class="flex justify-center">
                                     {{-- <a href="/superadmin/desa/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">S</a> --}}
-                                    {{-- <a href="/superadmin/desa/{{ $row['id'] }}/edit"
+                                    <a href="/superadmin/desa/{{ $row['id'] }}/edit"
                                         class=" focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"><svg
                                             class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                             width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -135,7 +135,7 @@
                                             <path fill-rule="evenodd"
                                                 d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z"
                                                 clip-rule="evenodd" />
-                                        </svg></a> --}}
+                                        </svg></a>
 
                                     <form action="/superadmin/desa/{{ $row['id'] }}" method="post"
                                         onsubmit="return confirm('yakin')">
